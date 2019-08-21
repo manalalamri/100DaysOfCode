324ac25adf2e5071cd2746aa665012a6ab6a5eab
wijdan1995 <wijdan.w.k@outlook.sa>
2019-08-21 19:23:43 +0300

    Done week one

---
+++ week-one/weekOne.js
@@ -0,0 +1,12 @@
+// To comment and uncomment your work in your code editor press Ctrl + Shift 
+document.write('<h1>Hello world!</h1>');
+
+document.write('<p>Starting 100 Days Of Code with <a href="https://twitter.com/saudidevorg">saudidevorg</a></p>')
+
+// document.write(5 + 2)
+
+const x = 10
+document.write('<p>Hello, this is my first challenge and I will do it ' + x + ' times. <br> I will do it, I will learn JavaScript.</p>')
+
+document.write('<p><strong>Week One Done</strong></p>')
+
